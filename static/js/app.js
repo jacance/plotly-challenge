b500d48e7e09b6e4083c9468fe9ae61e4ed03a0e
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-02-03 01:01:06 -0800

    bar chart and demographic info and id filter solved

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,95 @@
 const url = "../samples.json";
 
 // Default function to be shown
-function buildAllcharts () {
+function buildAllcharts (id) {
 
     // Call data
-    d3.json(url).then((data) => {
+    d3.json(url).then((raw) => {
         // Sample values
-        console.log(data)
-        var sampleValues = data.samples.map(x => x.sample_values.slice(0,10));
-        // Test array
-        var sampleValues1 = sampleValues[0]
-        console.log(sampleValues)
-        console.log(sampleValues1)
+        console.log(raw)
 
-        var otuIds = data.samples.map(x=>x.otu_ids.slice(0,10));
-        // Test array
-        var otuIds1 = otuIds[0]
+        data = raw.samples.filter(d => d.id === id)
+        demoData = raw.metadata.filter(d => d.id == id)
+        console.log(demoData)
 
-        var otuLabels = data.samples.map(x=>x.otu_labels.slice(0,10));
-        // Test array
-        var otuLabels1 = otuLabels[0]
+        // Slice 10 sampleValues for plots
+        var sampleValues = data.map(x => x.sample_values.slice(0,10))[0];
+    
+        // Slice 10 otuIds for plots
+        var otuIds = data.map(x=>x.otu_ids.slice(0,10))[0];
+
+        // Slice 10 otuLabels for plots
+        var otuLabels = data.map(x=>x.otu_labels.slice(0,10))[0];
+
+        
         var ylabels = []
+
         // Convert OTU IDs to string
-        for (let i =0; i < otuIds1.length; i++) {
-            otuString = otuIds1[i].toString()
+        for (let i =0; i < otuIds.length; i++) {
+            otuString = otuIds[i].toString()
             ylabels.push(`OTU ${otuString}`)
         }
-        // Move into function
-        var trace = {
-            x: sampleValues1,
-            y: ylabels,
-            type: "bar",
-            orientation: "h",
-            text: otuLabels1
-        };
 
-        var layout = {
-            xaxis: {tick0: 0, dtick: 50},
-            yaxis: {autorange: "reversed"}
-            
-        }
-
-        data = [trace]
-        Plotly.newPlot("bar", data, layout)
+        buildBarCharts(sampleValues, ylabels, otuLabels)
+        demoInfo(demoData)
         
-    });
-}
+    })
+};
 
-// Already in index.html
+// Event listener updates dashboard based on user selection
 function optionChanged (id) {
-    filtered = data.filter(d => d.id ===id);
-    buildChats(filtered);
+    buildAllcharts(id);
 
-}
+};
 
+// Function for drop down menu
 function init(){
-    d3.select("selDataset")
-}
+     var html = ""
+     d3.json(url).then(function(d){
+        d.names.map(function(each){
+            html += "<option value = "+each+" > "+each+"</option>"
+        })
+        //  console.log(html);
+         document.getElementById("selDataset").innerHTML = html;
+         optionChanged(d.names[0]);
+     });
+    }
+
+// Function for bar charts
+function buildBarCharts (xvalues, yvalues, textvalues) {
+    var trace = {
+        x: xvalues,
+        y: yvalues,
+        type: "bar",
+        orientation: "h",
+        text: textvalues
+    };
+    var layout = {
+        xaxis: {tick0: 0, dtick: 50},
+        yaxis: {autorange: "reversed"}
+        
+    }
+
+    data = [trace]
+    Plotly.newPlot("bar", data, layout)
+};
+
+// Function for demographic info / metadata
+function demoInfo (filteredData) {
+    var div = d3.select("#sample-metadata").html("")
+    filteredData.forEach(d => {
+        Object.entries(d).forEach(([k,v]) => {
+            var row = div.append("div")
+            row.text(`${k}: ${v}`)
+            console.log(k, v)
+        })
+    })
+
+};
+
+    
 
 
+// Initialize
+init();
 
-
-barChart()
-// Function to change plots based on dropdown
