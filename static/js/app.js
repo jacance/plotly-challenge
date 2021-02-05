2b85dd4da3a6ec1f7819b38e1b788ebfce97e3b4
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-02-04 23:56:13 -0800

    charts done

--- static/js/app.js
+++ static/js/app.js
@@ -12,10 +12,10 @@ function buildAllcharts (id) {
         demoData = raw.metadata.filter(d => d.id == id)
         console.log(demoData)
 
-        // Slice 10 sampleValues for plots
+        // Slice 10 sampleValues for bar chart
         var sampleValues = data.map(x => x.sample_values.slice(0,10))[0];
     
-        // Slice 10 otuIds for plots
+        // Slice 10 otuIds for bar chart
         var otuIds = data.map(x=>x.otu_ids.slice(0,10))[0];
 
         // Slice 10 otuLabels for plots
@@ -30,8 +30,23 @@ function buildAllcharts (id) {
             ylabels.push(`OTU ${otuString}`)
         }
 
-        buildBarCharts(sampleValues, ylabels, otuLabels)
+        // Sample values for bubble chart
+        var sampleValues1 = data.map(x => x.sample_values)[0];
+    
+        // otuIds for bubble chart
+        var otuIds1 = data.map(x=>x.otu_ids)[0];
+
+        // otuLabels for bubble chart
+        var otuLabels1 = data.map(x=>x.otu_labels)[0]
+
+
+        var washFreq = demoData.map(x=>x.wfreq)[0]
+
+
+        buildBarChart(sampleValues, ylabels, otuLabels)
+        buildBubbleCharts(otuIds1, sampleValues1, sampleValues1, otuIds1, otuLabels1)
         demoInfo(demoData)
+        gaugeChart(washFreq)
         
     })
 };
@@ -55,8 +70,8 @@ function init(){
      });
     }
 
-// Function for bar charts
-function buildBarCharts (xvalues, yvalues, textvalues) {
+// Function for bar chart
+function buildBarChart (xvalues, yvalues, textvalues) {
     var trace = {
         x: xvalues,
         y: yvalues,
@@ -74,6 +89,32 @@ function buildBarCharts (xvalues, yvalues, textvalues) {
     Plotly.newPlot("bar", data, layout)
 };
 
+// Function for bubble chart
+function buildBubbleCharts (xvalues, yvalues, markerSize, markerColors, textValues) {
+    var trace = {
+        x: xvalues,
+        y: yvalues,
+        mode: "markers",
+        marker: {
+            size: markerSize,
+            color: markerColors,
+        },
+        text: textValues,
+    };
+
+    var data = [trace]
+
+    var layout = {
+        xaxis: {
+            title: "OTU ID"
+        },
+        showlegend: false,
+        height: 500,
+        width: 1200
+    }
+    Plotly.newPlot("bubble", data, layout)
+};
+
 // Function for demographic info / metadata
 function demoInfo (filteredData) {
     var div = d3.select("#sample-metadata").html("")
@@ -87,9 +128,48 @@ function demoInfo (filteredData) {
 
 };
 
+// Function for Gauge Chart
+function gaugeChart (value) {
+    var data = [
+        {
+        type: "indicator",
+        mode: "gauge+number",
+        value: value,
+        title: { text: "Scrubs Per Week", font: { size: 18 } },
+        delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+        gauge: {
+            axis: { range: [null, 10]},
+            bar: { color: "darkred" },
+            bgcolor: "white",
+            borderwidth: 2,
+            bordercolor: "gray",
+            steps: [
+            { range: [0, 1], color: "#FBFBEF" },
+            { range: [1, 2], color: "#F9F9E3" },
+            { range: [2, 3], color: "#F1FDCC" },
+            { range: [3, 4], color: "#DDF5C8" },
+            { range: [4, 5], color: "#CAF998" },
+            { range: [5, 6], color: "#9CDF55" },
+            { range: [6, 7], color: "#7FC436" },
+            { range: [7, 8], color: "#63AF4F" },
+            { range: [8, 9], color: "#3B9622" },
+            { range: [9, 10], color: "darkgreen" },
+        
+            ],
+        }
+        }
+    ];
     
+    var layout = {
+        width: 450,
+        height: 200,
+        margin: { t: 40, r: 25, l: 25, b: 25 },
+    };
+    
+    Plotly.newPlot('gauge', data, layout);
+};
 
-
+    
 // Initialize
 init();
 
